7c8afb932af37e99da00ab51ac813dcd6a62b7fc
superliuxz <superliuxz@gmail.com>
2017-02-08 21:39:01 +0000

    Add HTCondorToCsv.sh

---
+++ HTCondorToCsv.sh
@@ -0,0 +1,15 @@
+preOS=$1
+condHis=$2
+
+col="QDate Requirements Owner VMLoc VMInstanceType RequestMemory RequestDisk MemoryUsage DiskUsage LastRemoteHost DiskUsage ImageSize ResidentSetSize \
+JobStatus CommittedTime CommittedSuspensionTime CumulativeSlotTime NumJobStarts RequestCpus JobStartDate VMName RemoveReason CompletionDate"
+
+extra="VMCPUCores VMMem VMStorage"
+
+if [ "$preOS" == "-pre" ]; then
+	condor_history -file $condHis -af:,rh $col $extra > $condHis".csv"
+elif [ "$preOS" == "-post" ]; then
+	condor_history -file $condHis -af:,rh $col > $condHis".csv"
+else
+	echo "usage: $ condToCsv.sh -pre/-post HTCONDOR_HISTORY_FILE"
+fi	
