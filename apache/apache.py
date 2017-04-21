9c4c2279289adcea99390420dcbc4a5ab6f5d606
superliuxz <superliuxz@gmail.com>
2017-04-21 09:41:11 -0700

    Add meeting service, delete community page hits

--- apache/apache.py
+++ apache/apache.py
@@ -35,13 +35,13 @@ class Init():
 		return Elasticsearch(self.url, timeout = self.timeout)
 
 def fig1(idx, conn, svc):
-	service = {"ssos" : ["ssos", "ssosclf.pl"], "dss" : ["dss", "dss_status.py"], "community": ["community"]}
-	ttl = {"ssos" : "Solar System Object Image Search", "dss" : "Digital Sky Survey System", "community" : "Community"}
+	service = {"ssos" : ["ssos", "ssosclf.pl"], "dss" : ["dss", "dss_status.py"], "meeting": ["getMeetings.html", "meetingsvc"]}
+	ttl = {"ssos" : "Solar System Object Image Search", "dss" : "Digital Sky Survey System", "meeting" : "Meetings"}
 	plts = [Div(text = "<h1>{}</h1>".format(ttl[svc]), width = 1000)]
 	for req in service[svc]:
 		df = pd.DataFrame()
 		df2 = pd.DataFrame()
-		for dom in ["CC", "CADC", "NRC", "Others"]:
+		for dom in ["internal", "external"]:
 			query = {
 				"size" : 0,
 				"query" : {
@@ -68,24 +68,14 @@ def fig1(idx, conn, svc):
 					}
 				}
 			}
-			if svc == "community":
-				query["query"]["bool"].setdefault("must", []).append({ "range" : { "@timestamp" : { "gte" : "2014-12"} } })
-			if dom == "CADC":
+			if svc == "meeting" and req != service[svc][0] :
+				query["query"]["bool"].setdefault("must", []).append({ "match" : { "referrer" : "editMeetings.html" } })
+			if dom == "internal":
 				query["query"]["bool"]["minimum_should_match"] = 1
 				query["query"]["bool"]["should"] = [
-						{ "term" : { "geoip.ip" : "132.246.194.0/24"}},
-						{ "term" : { "geoip.ip" : "132.246.195.0/24"}},
-						{ "term" : { "geoip.ip" : "132.246.217.0/24"}},
-						{ "term" : { "geoip.ip" : "192.168.0.0/16"}}
-					]
-			elif dom == "CC":
-				query["query"]["bool"].setdefault("must", []).append({ "term" : { "geoip.ip" : "206.12.0.0/16" } })
-			elif dom == "NRC":
-				query["query"]["bool"].setdefault("must", []).append({ "term" : { "geoip.ip" : "132.246.0.0/16" } })
-				query["query"]["bool"]["must_not"] = [
-					{ "term" : { "geoip.ip" : "132.246.194.0/24"}},
-					{ "term" : { "geoip.ip" : "132.246.195.0/24"}},
-					{ "term" : { "geoip.ip" : "132.246.217.0/24"}}
+					{ "term" : { "geoip.ip" : "132.246.0.0/16"}},
+					{ "term" : { "geoip.ip" : "206.12.0.0/16"}},
+					{ "term" : { "geoip.ip" : "192.168.0.0/16"}}
 				]
 			else:
 				query["query"]["bool"]["must_not"] = [
@@ -94,48 +84,33 @@ def fig1(idx, conn, svc):
 					{ "term" : { "geoip.ip" : "206.12.0.0/16"}},
 					{ "term" : { "geoip.ip" : "192.168.0.0/16"}}
 				]
-				#print(json.dumps(query, indent = 4))
+				
 			res = conn.search(index = idx, body = query)
 
 			for _ in res["aggregations"]["peryr"]["buckets"]:
 			 	df = df.append(pd.DataFrame([[_["doc_count"]]], columns = [dom], index = [_["key_as_string"]]))
 			 	df2 = df2.append(pd.DataFrame([[_["unq_ip"]["value"]]], columns = [dom], index = [_["key_as_string"]])) 	
 
-		df = df.groupby(df.index).sum().fillna(0).apply(np.log10).replace([np.inf, -np.inf], np.nan).reset_index()
-		df = df.rename(columns = {"index":"time"})
+		for _ in [df, df2]:
+			_ = _.groupby(_.index).sum().fillna(0).reset_index()
+			_ = _.rename(columns = {"index":"time"})
 
-		p = Bar(data = df, 
-				values = blend("CADC", "NRC", "CC", "Others", name = "Page Hits", labels_name = "domain"), 
-				label = cat(columns = "time", sort = False), 
-				stack = cat(columns = "domain", sort = False), 
-				color = color(columns = "domain", palette = clr, sort = False), 
-				legend = "top_right", title = "{}".format("Page Hits" if req == service[svc][0] else "Service Use"), 
-				tooltips = [("domain", "@domain"), ("time", "@time"), ("hits", "@height")],
-				plot_width = 1200
-				)
-		p.yaxis.axis_label = "Log10 of {}".format("Page Hits" if req == service[svc][0] else "Service Use")
-		plts.append(p)
-
-		df2 = df2.groupby(df2.index).sum().fillna(0).reset_index()
-		df2 = df2.rename(columns = {"index":"time"})
-		#print(df2)
-
-		p = Bar(data = df2, 
-				values = blend("CADC", "NRC", "CC", "Others", name = "Unique IPs", labels_name = "domain"), 
-				label = cat(columns = "time", sort = False), 
-				stack = cat(columns = "domain", sort = False), 
-				color = color(columns = "domain", palette = clr, sort = False), 
-				legend = "top_right", title = "{} Unique IPs".format("Page Hits" if req == service[svc][0] else "Service Use"), 
-				tooltips = [("domain", "@domain"), ("time", "@time"), ("hits", "@height")],
-				plot_width = 1200
-				)
-		p.yaxis.axis_label = "Number of {} Unique IPs".format("Page Hits" if req == service[svc][0] else "Service Use")
-		plts.append(p)
+			p = Bar(data = _, 
+					values = blend("external", "internal", name = "Page Hits", labels_name = "domain"), 
+					label = cat(columns = "time", sort = False), 
+					stack = cat(columns = "domain", sort = False), 
+					color = color(columns = "domain", palette = clr, sort = False), 
+					legend = "top_right", title = "{}".format("Page Hits" if req == service[svc][0] else "Service Use"), 
+					tooltips = [("domain", "@domain"), ("time", "@time"), ("hits", "@height")],
+					plot_width = 1200
+					)
+			p.yaxis.axis_label = "Log10 of {}".format("Page Hits" if req == service[svc][0] else "Service Use")
+			plts.append(p)
 
 	output_file("fig1.html")
 	show(column([plts[0]] + plts[1::2] + plts[2::2]))
 	
 if __name__ == "__main__":
 	conn = Init().connect()
-	fig1("logs-apache", conn, "community")
+	fig1("logs-apache", conn, "meeting")
 
